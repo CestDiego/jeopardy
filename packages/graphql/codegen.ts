9b2a2830c7c06ac9539438f013edd3d2c56e0e82
Giorgio Leveroni <ppold@users.noreply.github.com>
2024-10-04 13:14:39 -0500

    Add codegen

---
+++ packages/graphql/codegen.ts
@@ -0,0 +1,17 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+import { printSchema } from 'graphql'
+import { schema } from '../functions/src/graphql/schema'
+
+const config: CodegenConfig = {
+  schema: printSchema(schema),
+  generates: {
+    'schema.graphql': {
+      plugins: ['schema-ast'],
+      hooks: {
+        afterOneFileWrite: ['npx @genql/cli --esm --output ./genql --schema'],
+      },
+    },
+  },
+}
+
+export default config
